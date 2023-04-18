f480ff3891498cb1c4e124a1d67505cfec1b852b
Bright <kumi.bright@yahoo.com>
2023-04-18 09:42:23 -1200

    adding all files

---
+++ Start/script.js
@@ -0,0 +1,10 @@
+// const agian = document.querySelector("#page");
+// // const pointu = document.querySelector("#onpoint");
+// const link = "window.location.href = 'succesPage.html'";
+
+// var button = document.querySelector("#btn");
+// button.addEventListener("click", () => {
+//   //   document.write("Hello World");
+//   button = link;
+//   console.log(agian);
+// });
